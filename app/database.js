170d9af396d55172fb80a243df6809cbb2647f94
hiihbcb <diabloeatmice@gmail.com>
2021-02-20 11:06:34 +0000

    update tables

--- app/database.js
+++ app/database.js
@@ -13,6 +13,7 @@ const dbClient = new pg({
   connectionString: databaseUrl,
   ssl: sslConfig
 });
+
 dbClient.connect(err => {
   if (err) {
     console.error('connection error', err.stack)
@@ -32,17 +33,22 @@ class Database {
 
     generateTable(tableName, tableColumns) {
         var table = "CREATE TABLE IF NOT EXISTS " + tableName + " (";
-        for (var column in tableColumns) {
-            if (column == tableColumns.length - 1) {
-                table += tableColumns[column] + ");";
-            } else {
-                table += tableColumns[column] + ", ";
-            }
-        }
+        table = this.concatColumn(table, tableColumns);
 
         dbClient.query(table)
                 .then(res => console.log(table))
                 .catch(e => console.error(e.stack));
+    }
+
+    concatColumn(text, values, endOfLine = ");", seperator = ", ") {
+        for (var value in values) {
+            if (value == values.length - 1) {
+                text += values[value] + endOfLine;
+            } else {
+                text += values[value] + seperator;
+            }
+        }
+        return text;
     }
 }
 
